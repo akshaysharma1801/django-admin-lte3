dd22a98514e0f06b27fcb3d93a92544ba2c3ec0f
Akshay Sharma <akshay.sharma@logixgrid.com>
2020-12-18 01:28:38 +0530

    accounts views done

--- django_admin3/apps/accounts/urls.py
+++ django_admin3/apps/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path,include
-from .views import RegisterView,LoginView
+from .views import RegisterView
+from .views import LoginView
+from .views import ForgotPasswordView
+
 
 app_name = 'accounts'
+
 urlpatterns = [
     path('', LoginView.as_view(),name='login'),
     path('register/', RegisterView.as_view(),name='register'),
+    path('forgot-password/', ForgotPasswordView.as_view(),name='forgot-password'),
 ]
--- django_admin3/apps/accounts/views.py
+++ django_admin3/apps/accounts/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render, redirect, reverse
 from django.views import View
 
-# Create your views here.
+
 class LoginView(View):
+    ''' User Login view. '''
 
     template_name = 'accounts/login.html'
 
     def get(self, request):
         return render(request, self.template_name)
 
+    def post(self, request):
+        pass    
+
 
 class RegisterView(View):
+    ''' User register view. '''
+
+    template_name = 'accounts/register.html'
 
     def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
         pass
+
+class ForgotPasswordView(View):
+    ''' User forgot password view. '''
+
+    template_name = 'accounts/forgot-password.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
 
     def post(self, request):
         pass
